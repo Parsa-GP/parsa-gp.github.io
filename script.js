fd0e076f47e8cf9775d681f0d4edc13fdbad8bdd
parsa <psrzp1386@gmail.com>
2024-10-07 06:38:45 +0330

    Adding posts & js overhaul ## script.js - Removed all ";" + Adding articles * Better variable name of post elements

--- script.js
+++ script.js
@@ -1,106 +1,144 @@
-let api = "/gh-blog";
+let api = "/gh-blog"
 
 
 function home() {
-    alert("Home!!")
+		alert("Home!!")
 }
 
 function github() {
-    window.location.href = "https://github.com/parsa-gp";
+		window.location.href = "https://github.com/parsa-gp"
 }
 
 function about() {
-    alert("About!!")
+		alert("About!!")
 }
 
 function pall() {
-    alert("All Posts!!")
+		alert("All Posts!!")
 }
 
 
 async function fetchData() {
-  try {
-    const response = await fetch(new URL(api+'/data.json', window.location.href).href);
-    if (!response.ok) {
-      throw new Error(`HTTP error! status: ${response.status}`);
-    }
-    const data = await response.json();
-    console.log("Data fetched successfully!");
-    loopThroughData(data);
-  } catch (error) {
-    console.error('Error fetching data:', error);
-  }
+	try {
+		const response = await fetch(new URL(api+'/data.json', window.location.href).href)
+		if (!response.ok) {
+			throw new Error(`HTTP error! status: ${response.status}`)
+		}
+		const data = await response.json()
+		console.log("Data fetched successfully!")
+		loopThroughData(data)
+	} catch (error) {
+		console.error('Error fetching data:', error)
+	}
 }
 
 function loopThroughData(data) {
-  for (const item of data) {
-    const { title, category, date, images, description } = item;
+	for (const item of data) {
+		const { type, title, category, date, images, description, content } = item
+
+		post = document.createElement("div")
+		post.classList = ["post"]
+
+		post_topbar = document.createElement("div")
+		post_topbar.classList = ["p-top"]
+
+		post_title = document.createElement("span")
+		post_title.classList = ["p-title"]
+
+		post_tag_container = document.createElement("div")
+		post_tag_container.classList = ["p-tags"]
+
+		post_tag = document.createElement("span")
+		post_tag.classList = ["p-tag"]
+
+		post_time = document.createElement("span")
+		post_time.classList = ["p-time"]
+
+
+
+		post_title.innerHTML = title
+		post_tag.innerHTML = category
+		post_time.innerHTML = date
+
+
+		post_tag_container.appendChild(post_tag)
+		post_tag_container.appendChild(post_time)
+
+		post_topbar.appendChild(post_tag_container)
+		post_topbar.appendChild(post_title)
+
+		post.appendChild(post_topbar)
+
+
+		if (typeof content != "undefined") {
+			pcontent = document.createElement("span")
+			pcontent.classList = ["p-text"]
+			pcontent.innerHTML = content
+			post.appendChild(pcontent)
+		}
+
+		if (typeof description != "undefined") {
+			pdesc = document.createElement("span")
+			pdesc.classList = ["p-text"]
+			pdesc.innerHTML = description
+			post.appendChild(pdesc)
+		}
+
+		if (images.length!=0) {
+			pimgs = document.createElement("div")
+			pimgs.classList = ["p-images"]
+			post.appendChild(pimgs)
+			
+			for (const img of images) {
+					pimg = document.createElement("img")
+					pimg.classList = ["p-img"]
+					pimg.src = api+"/assets/"+img
+					pimg.loading="lazy"
+					pimgs.appendChild(pimg)
+			}
+
+		}
 
 
 
 
-
-    post = document.createElement("div")
-    post.classList = ["post"];
-
-    ptop = document.createElement("div")
-    ptop.classList = ["p-top"];
-
-    ptitle = document.createElement("span")
-    ptitle.classList = ["p-title"];
-
-    ptags = document.createElement("div")
-    ptags.classList = ["p-tags"];
-
-    ptag = document.createElement("span")
-    ptag.classList = ["p-tag"];
-
-    ptime = document.createElement("span")
-    ptime.classList = ["p-time"];
-
-    pimgs = document.createElement("div")
-    pimgs.classList = ["p-images"];
-
-    pdesc = document.createElement("span")
-    pdesc.classList = ["p-desc"];
-
-    ptitle.innerHTML = title
-    ptag.innerHTML = category
-    ptime.innerHTML = date
-    pdesc.innerHTML = description
-
-
-    ptags.appendChild(ptag)
-    ptags.appendChild(ptime)
-
-    ptop.appendChild(ptags)
-    ptop.appendChild(ptitle)
-
-    post.appendChild(ptop)
-    post.appendChild(pimgs)
-    post.appendChild(pdesc)
-
-
-    for (const img of images) {
-        pimg = document.createElement("img")
-        pimg.classList = ["p-img"];
-        pimg.src = api+"/assets/"+img;
-        pimg.loading="lazy"
-        pimgs.appendChild(pimg)
-    }
-
-
-    cont = document.getElementById("container")
-    cont.appendChild(post)
-  }
+		cont = document.getElementById("container")
+		cont.appendChild(post)
+	}
 }
-document.addEventListener('DOMContentLoaded', () => {
-  let hue = 0;
-  const increment = 0.75;
 
-  window.addEventListener('scroll', () => {
-      hue = (hue + increment) % 360;
-      document.documentElement.style.setProperty('--hue', hue);
-  });
-});
-fetchData();
+
+async function fetch_quotes() {
+	try {
+		const response = await fetch(new URL(api+"/quote.json", window.location.href).href)
+		if (!response.ok) {
+			throw new Error(`HTTP error! status: ${response.status}`)
+		}
+		const data = await response.json()
+		console.log("Quotes fetched successfully!")
+		quote_loader(data)
+	} catch (error) {
+		console.error('Error fetching data:', error)
+	}
+	
+}
+
+function quote_loader(q) {
+	rand = q[Math.floor(Math.random() * q.length)]
+	qe = document.getElementById("quote")
+	qe.innerHTML = "“ "+ rand.text + "  ”\n<br><br>\n - "+rand.name+", "+rand.year+" "
+}
+
+
+document.addEventListener('DOMContentLoaded', () => {
+	let hue = 0
+	const increment = 0.75
+
+	window.addEventListener('scroll', () => {
+			hue = (hue + increment) % 360
+			document.documentElement.style.setProperty('--hue', hue)
+	})
+})
+fetchData()
+fetch_quotes()
+
