b984c7a6168725ba03c2663a9ceffa9ae4e379a8
parsa <psrzp1386@gmail.com>
2024-10-18 13:08:36 +0330

    Bix Fugs + Adding links to social icons in header * Fix share.js * Scaling up post when hovered * Moving responsive code to home.css

--- scripts/share.js
+++ scripts/share.js
@@ -8,11 +8,11 @@ email = document.getElementsByClassName("email")[0]
 
 url = window.location.href
 
-twitter.href = "https://twitter.com/share?url="+url+"&text=This+article+BANGS!&via=MyUsername"
-facebook = "https://www.facebook.com/sharer/sharer.php?u="+url+""
-reddit = "https://reddit.com/submit?url="+url+"&title=This+article+BANGS!"
-hackernews = "https://news.ycombinator.com/submitlink?u="+url+"&t=This+article+BANGS!"
-linkedin = "https://www.linkedin.com/shareArticle?url="+url+"&title=This+article+BANGS!"
-email = "mailto:?subject=This+article+BANGS!&body="+url+"</URL>"
+twitter.href = "https://twitter.com/share?url="+url+"&text=This+article+BANGS!"
+facebook.href = "https://www.facebook.com/sharer/sharer.php?u="+url
+reddit.href = "https://reddit.com/submit?url="+url+"&title=This+article+BANGS!"
+hackernews.href = "https://news.ycombinator.com/submitlink?u="+url+"&t=This+article+BANGS!"
+linkedin.href = "https://www.linkedin.com/shareArticle?url="+url+"&title=This+article+BANGS!"
+email.href = "mailto:?subject=This+article+BANGS!&body="+url
 
 
